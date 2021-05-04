9a07c88a81ae62e456892784a29daa71d2a4d262
Jean Boussier <jean.boussier@gmail.com>
2021-05-04 13:25:13 +0200

    Fix ruby-head compatibility

--- Gemfile
+++ Gemfile
@@ -3,7 +3,7 @@ source "https://rubygems.org"
 # Specify your gem's dependencies in dedup.gemspec
 gemspec
 
-gem "rake", "~> 12.0"
+gem "rake", "~> 13"
 gem "rake-compiler"
 gem "minitest", "~> 5.0"
 gem "byebug"
--- ext/dedup/extconf.rb
+++ ext/dedup/extconf.rb
@@ -4,7 +4,7 @@ require 'mkmf'
 
 # FIXME: should use have_func("rb_hash_bulk_insert", ["ruby.h"])
 if RUBY_ENGINE == 'ruby' && RUBY_VERSION >= '2.7'
-  $CFLAGS = "-O3 -Wall"
+  $CFLAGS << " -O3 -Wall "
   create_makefile('dedup/dedup')
 else
   File.write("Makefile", dummy_makefile($srcdir).join(""))
